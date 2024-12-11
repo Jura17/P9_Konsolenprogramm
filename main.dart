95a0985a6288ab0bfe8eb652eacaeca417e8c3cf
Julian Rakow <julian.rakow@yahoo.com>
2024-12-11 17:39:36 +0100

    Implement functions addRecipe and addCookingDirections

--- main.dart
+++ main.dart
@@ -1,35 +1,33 @@
 import 'dart:io';
 
-
 Map chickenWings = {
     "title" : "Sesame Chicken Wings",
     "difficulty" : "Not too tricky",
     "preptime" : "40 min",
-    "directions" : "\n1. Place the eggs, salt and pepper in a bowl. Stir to combine\n2. Place the flour and 1/2 cup of cornstarch in a shallow bowl or on a plate. Stir to combine.\n3. Dip each piece of chicken into the egg mixture, then into the flour. Repeat the process with all of the chicken.\n4. Heat 3 inches of oil in a deep pan to 350 degrees F\n5. Add 7-8 pieces of chicken to the pan. Cook for 5 minutes or until crispy and golden brown. Repeat the process with the remaining chicken\n6. Drain the chicken on paper towels\n7. While the chicken is cooking, combine the honey, soy sauce, ketchup, brown sugar, rice vinegar, sesame oil and 2 teaspoons of cornstarch in a bowl.",
+    "directions" : ["Place the eggs, salt and pepper in a bowl. Stir to combine", "Place the flour and 1/2 cup of cornstarch in a shallow bowl or on a plate. Stir to combine.", "Dip each piece of chicken into the egg mixture, then into the flour. Repeat the process with all of the chicken.", "Heat 3 inches of oil in a deep pan to 350 degrees F", "Add 7-8 pieces of chicken to the pan. Cook for 5 minutes or until crispy and golden brown. Repeat the process with the remaining chicken", "Drain the chicken on paper towels", "While the chicken is cooking, combine the honey, soy sauce, ketchup, brown sugar, rice vinegar, sesame oil and 2 teaspoons of cornstarch in a bowl."],
   };
 
   Map shiitakeStirFry = {
     "title" : "Shiitake Stir-fry",
     "difficulty" : "Not too tricky",
     "preptime" : "40 min",
-    "directions" : "\n1. Place the eggs, salt and pepper in a bowl. Stir to combine\n2. Place the flour and 1/2 cup of cornstarch in a shallow bowl or on a plate. Stir to combine.\n3. Dip each piece of chicken into the egg mixture, then into the flour. Repeat the process with all of the chicken.\n4. Heat 3 inches of oil in a deep pan to 350 degrees F\n5. Add 7-8 pieces of chicken to the pan. Cook for 5 minutes or until crispy and golden brown. Repeat the process with the remaining chicken\n6. Drain the chicken on paper towels\n7. While the chicken is cooking, combine the honey, soy sauce, ketchup, brown sugar, rice vinegar, sesame oil and 2 teaspoons of cornstarch in a bowl.",
+    "directions" : ["Place the eggs, salt and pepper in a bowl. Stir to combine", "Place the flour and 1/2 cup of cornstarch in a shallow bowl or on a plate. Stir to combine.", "Dip each piece of chicken into the egg mixture, then into the flour. Repeat the process with all of the chicken.", "Heat 3 inches of oil in a deep pan to 350 degrees F", "Add 7-8 pieces of chicken to the pan. Cook for 5 minutes or until crispy and golden brown. Repeat the process with the remaining chicken", "Drain the chicken on paper towels", "While the chicken is cooking, combine the honey, soy sauce, ketchup, brown sugar, rice vinegar, sesame oil and 2 teaspoons of cornstarch in a bowl."],
   };
 
   Map threeIngredientPizza = {
     "title" : "3-Ingredient-Pizza",
     "difficulty" : "Not too tricky",
     "preptime" : "40 min",
-    "directions" : "\n1. Place the eggs, salt and pepper in a bowl. Stir to combine\n2. Place the flour and 1/2 cup of cornstarch in a shallow bowl or on a plate. Stir to combine.\n3. Dip each piece of chicken into the egg mixture, then into the flour. Repeat the process with all of the chicken.\n4. Heat 3 inches of oil in a deep pan to 350 degrees F\n5. Add 7-8 pieces of chicken to the pan. Cook for 5 minutes or until crispy and golden brown. Repeat the process with the remaining chicken\n6. Drain the chicken on paper towels\n7. While the chicken is cooking, combine the honey, soy sauce, ketchup, brown sugar, rice vinegar, sesame oil and 2 teaspoons of cornstarch in a bowl.",
+    "directions" : ["Place the eggs, salt and pepper in a bowl. Stir to combine", "Place the flour and 1/2 cup of cornstarch in a shallow bowl or on a plate. Stir to combine.", "Dip each piece of chicken into the egg mixture, then into the flour. Repeat the process with all of the chicken.", "Heat 3 inches of oil in a deep pan to 350 degrees F", "Add 7-8 pieces of chicken to the pan. Cook for 5 minutes or until crispy and golden brown. Repeat the process with the remaining chicken", "Drain the chicken on paper towels", "While the chicken is cooking, combine the honey, soy sauce, ketchup, brown sugar, rice vinegar, sesame oil and 2 teaspoons of cornstarch in a bowl."],
   };
 
   List<Map> allRecipes = [chickenWings, shiitakeStirFry, threeIngredientPizza];
   // List<Map> favoriteRecipes = [shiitakeStirFry, threeIngredientPizza];
 
 void main(List<String> args) {
-  bool isRunningProgram = true;
 
-  while(isRunningProgram) {
-    print("\n-------- MAIN MENU --------");
+  while(true) {
+    print("\n======== MAIN MENU ========");
     print("[1] Cookbook");
     print("[2] Recommend Me Something");
     print("[3] Shopping List");
@@ -46,7 +44,7 @@ void main(List<String> args) {
         shoppingListSubMenu();
       case "x":
         print("Terminating program...\n");  
-        isRunningProgram = false;
+        return;
       default:
         print("Invalid input, please try again.");
     }
@@ -55,10 +53,9 @@ void main(List<String> args) {
 }
 
 void recipeSubMenu() {
-  bool isShowingRecipeSubMenu = true;
 
-  while(isShowingRecipeSubMenu) {
-    print("\n-------- RECIPE SUBMENU --------");
+  while(true) {
+    print("\n======== RECIPE SUBMENU ========");
     print("[1] Show all recipes");
     print("[2] Add a recipe");
     print("[3] Delete recipe");
@@ -70,12 +67,12 @@ void recipeSubMenu() {
       case "1":
         showAllRecipes(allRecipes);
       case "2":  
-        print("Prompting user add info about a recipe...");
+        addRecipe();
       case "3":
         print("Showing a list of recipe titles to choose from for deletion...");
       case "x":
         print("Terminating function call...\n");  
-        isShowingRecipeSubMenu = false;
+        return;
       default:
         print("Invalid input, please try again.");
     }
@@ -84,17 +81,16 @@ void recipeSubMenu() {
 }
 
 void recommendationsSubMenu() {
-  print("\n-------- RECOMMENDATIONS --------");
+  print("\n======== RECOMMENDATIONS ========");
   print("Asking user several questions...");
   print("...");
   print("Showing recommendation...");
 }
 
 void shoppingListSubMenu() {
-  bool isShowingShoppingListSubMenu = true;
 
-  while(isShowingShoppingListSubMenu) {
-    print("\n-------- SHOPPING LIST SUBMENU --------");
+  while(true) {
+    print("\n======== SHOPPING LIST SUBMENU ========");
     print("[1] Show all shopping list items");
     print("[2] Add a item");
     print("[3] Delete item");
@@ -111,7 +107,7 @@ void shoppingListSubMenu() {
         print("Showing a list of items to choose from for deletion...");
       case "x":
         print("Terminating function call...\n");  
-        isShowingShoppingListSubMenu = false;
+        return;
       default:
         print("Invalid input, please try again.");
     }
@@ -123,7 +119,7 @@ void showAllRecipes(List<Map>recipeList) {
   bool isShowingAllRecipes = true;
 
   while(isShowingAllRecipes) {
-    print("=========================");
+    print("\n======== YOUR RECIPES ========");
     for (var i = 0; i < recipeList.length; i++) {
       print("${[i+1]} ${recipeList[i]["title"]}");
     }
@@ -151,10 +147,56 @@ void showAllRecipes(List<Map>recipeList) {
 }
 
 void showRecipeDetails(Map recipe) {
-    print("=========================");
+    print("-------------------------");
     print("Title: ${recipe["title"]}");
     print("Difficulty: ${recipe["difficulty"]}");
     print("Prep time: ${recipe["preptime"]}");
-    print("Directions: ${recipe["directions"]}");
-    print("=========================");
+    print("Directions:");
+    for (var i = 0; i < recipe["directions"].length; i++) {
+      print("${i + 1}. ${recipe["directions"][i]}");
+    }
+    print("-------------------------");
+}
+
+// Fragt den Nutzer nach einem Titel, Schwierigkeitsgrad, Zubereitungszeit insgesamt und nach einzelnen Zubereitungsschritten
+// Da Nutzer Fließtext eingeben soll, müssen auch Umbrüche möglich sein, ohne das Enter gedrückt wird. Hierfür soll einfach /L/ eingegeben werden. 
+// Der Compiler geht anschließend durch den gesamten String durch und fügt bei jedem /L/ einen Umbruch hinzu.
+// Daten werden in einer Map gespeichert
+// Fertige Map wird am Ende allRecipes list hinzugefügt 
+void addRecipe() {
+  print("\n======== ADD RECIPE ========");
+  print("Please enter the following infos about your recipe.");
+  stdout.write("Title: ");
+  String titleInput = stdin.readLineSync()!;
+  stdout.write("Difficulty: ");
+  String difficultyInput = stdin.readLineSync()!;
+  stdout.write("Prep time in total: ");
+  String prepTimeInput = stdin.readLineSync()!;
+  print("Directions (enter x when you're done): ");
+  List<String> directionsInput = addCookingDirections();
+
+  Map<String, dynamic> newRecipe = {
+    "title" : titleInput,
+    "difficulty" : difficultyInput,
+    "preptime" : prepTimeInput,
+    "directions" : directionsInput
+  };
+  allRecipes.add(newRecipe);
+  print("$titleInput was added to your recipe list");
+}
+
+List<String> addCookingDirections() {
+  List<String> userInput = [];
+  int stepCounter = 1;
+
+  while(true) {
+    stdout.write("$stepCounter: ");
+    String stepInput = stdin.readLineSync()!;
+    if(stepInput.toLowerCase() == "x") {
+      return userInput;
+    } else {
+      userInput.add(stepInput);
+      stepCounter++;
+    }
+  }
 }
